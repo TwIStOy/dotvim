5339d6b83c5da01924c1972b921250226cef6e5c
Hawtian Wang <twistoy.wang@gmail.com>
2019-05-30 11:08:35 +0800

    feat(crate::snippet): update shortname impl

--- pythonx/cpp_snippet_helper.py
+++ pythonx/cpp_snippet_helper.py
@@ -1,35 +1,43 @@
-def cpp_type_fullname(s):
-    return {
+def short_nameset_parse(s):
+    def p(*args):
+        if len(args) == 2:
+            tp = 'std::pair<{}>'
+        else:
+            tp = 'std::tuple<{}>'
+        return tp.format(", ".join(args))
+
+    simple_types = ['i', 'u', 's']
+    long_name_types = {
         'i': 'int32_t',
         'u': 'uint32_t',
         'ii': 'int64_t',
         'uu': 'uint64_t',
         's': 'std::string',
-    }.get(s, s)
+        'i8': 'int8_t',
+        'i16': 'int16_t',
+        'i32': 'int32_t',
+        'i64': 'int64_t',
+        'u8': 'uint8_t',
+        'u16': 'uint16_t',
+        'u32': 'uint32_t',
+        'u64': 'uint64_t'
+    }
 
-# pii
-# piu
-# puu
-# psu
-# pus
-# pii
-# p(u,s)
-# p(i,i)
-# p(i, i)
-# p(pii,i)
-def short_nameset_parse(s):
-    import re
-    def simple(ss):
-        return 'std::pair<{}, {}>'.format(cpp_type_fullname(ss[0]),
-                                          cpp_type_fullname(ss[1]))
-    if len(s) == 2:
-        return simple(s)
+    def cpp_type_fullname(s):
+        return long_name_types.get(s, s)
 
-    args = re.compile('\((.*)\)').match(s).group(1)
-    args = [item.strip() for item in args.split(',')]
-    args = [simple(item[1:]) if len(item) == 3 and item[0] == 'p'
-            else cpp_type_fullname(item) for item in args]
+    gbl = {}
+    gbl['p'] = p
+    for k1 in simple_types:
+        for k2 in simple_types:
+            gbl['p{}{}'.format(k1, k2)] = p(cpp_type_fullname(k1),
+                                            cpp_type_fullname(k2))
 
-    return "{}<{}>".format( 'std::pair' if len(args) == 2 else 'std::tuple',
-                           ", ".join(args))
+    gbl.update(long_name_types)
+
+    try:
+        return eval(s, gbl)
+    except:
+        return s
+
 

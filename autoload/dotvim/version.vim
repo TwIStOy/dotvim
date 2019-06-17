a1468871f50e42903fa45b701436a0649c4b89bd
Hawtian Wang <twistoy.wang@gmail.com>
2019-06-17 12:30:30 +0800

    feat(snippets): add greate ultisnips impl

--- autoload/dotvim/version.vim
+++ autoload/dotvim/version.vim
@@ -1,4 +1,4 @@
-let g:dotvim_last_updated_time = '2019.06.06'
+let g:dotvim_last_updated_time = '2019.06.17'
 
 function! dotvim#version#version() abort
   return g:dotvim_last_updated_time
--- pythonx/cpp_snippet_helper.py
+++ pythonx/cpp_snippet_helper.py
@@ -43,3 +43,34 @@ def short_nameset_parse(s):
 
 def underscore_to_big_camel_case(s):
     return "".join([i.capitalize() for i in s.split('_')])
+
+def function_helpers(s, snip):
+    gbl = {}
+    def namespace_generator(*args):
+        nonlocal snip
+        lines = []
+        args = list(args)
+        for name in args:
+            lines.append(f'namespace {name} ' + '{')
+        lines.append('${0: /* body */}')
+        args.reverse()
+        for name in args:
+            lines.append('}' + f'  // namespace {name}')
+        return "\n".join(lines)
+
+    gbl['ns'] = namespace_generator
+
+    try:
+        snip.rv = eval(s,gbl)
+        # UltiSnips_Manager.expand_anon(tmp)
+    except:
+        snip.rv = s
+
+def expand_anon(snip):
+    print(snip.snippet_start, snip.snippet_end)
+    anon = []
+    for i in range(snip.snippet_start[0], snip.snippet_end[0] + 1):
+        anon.append(snip.buffer[i])
+    snip.buffer[snip.snippet_start[0]:snip.snippet_end[0] + 1] = ['']
+    snip.expand_anon('\n'.join(anon))
+

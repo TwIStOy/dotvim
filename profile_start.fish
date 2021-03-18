dff965ced9e0147f3ac26e0d6a17e09892db0b90
Hawtian Wang <twistoy.wang@gmail.com>
2021-03-18 16:44:28 +0800

    feat(scirpt): upddate profile_start script

--- profile_start.fish
+++ profile_start.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 
-nvim --cmd "profile start ~/vimprofile.result" --cmd "profile! file *dotvim/*" $argv
+nvim --cmd "profile start ~/vimprofile.result" --cmd "profile! file *" --cmd 'profile! func *' $argv
 

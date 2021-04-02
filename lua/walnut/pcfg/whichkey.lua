3c98eefa3c67e092dc1426d9e0cb60517d1f991c
Hawtian Wang <twistoy.wang@gmail.com>
2021-04-02 17:27:40 +0800

    refactor(*): impl keymap in whichkey

--- lua/walnut/pcfg/whichkey.lua
+++ lua/walnut/pcfg/whichkey.lua
@@ -6,4 +6,7 @@ set_g_var('which_key_use_floating_win', 1)
 set_g_var('which_key_floating_relative_win', 1)
 set_g_var('which_key_centered', 0)
 
+vim.api.nvim_call_function('which_key#register', {
+    '<Space>', 'b:which_key_map'
+})
 

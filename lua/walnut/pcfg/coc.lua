be66e1520d13ac97688a900be1615fe13698637c
Hawtian Wang <twistoy.wang@gmail.com>
2021-04-02 16:41:10 +0800

    fix

--- lua/walnut/pcfg/coc.lua
+++ lua/walnut/pcfg/coc.lua
@@ -5,12 +5,16 @@ local ftdesc_folder = require('walnut.keymap').ftdesc_folder
 local cmd = vim.api.nvim_command
 local keymap = vim.api.nvim_set_keymap
 
-cmd [[au CursorHoldI,CursorMovedI * silent! call CocActionAsync('showSignatureHelp')]]
-cmd [[au! CompleteDone * if pumvisible() == 0 | pclose | endif ]]
+function setup()
+  vim.g.coc_start_at_startup = 0
+  vim.api.nvim_set_option('updatetime', 200)
+end
 
-vim.g.coc_start_at_startup = 0
-vim.api.nvim_set_option('updatetime', 200)
+function config()
+  cmd [[au CursorHoldI,CursorMovedI * silent! call CocActionAsync('showSignatureHelp')]]
+  cmd [[au! CompleteDone * if pumvisible() == 0 | pclose | endif ]]
 
-cmd [[source ~/.dotvim/pcfg/coc.vim]]
+  cmd [[source ~/.dotvim/pcfg/coc.vim]]
+end
 
 

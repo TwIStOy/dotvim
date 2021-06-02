2d0cdbd865716a745003a5eac39a95065f982a8c
Hawtian Wang <twistoy.wang@gmail.com>
2021-06-02 17:22:01 +0800

    update init.lua sample

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 vim.api.nvim_command [[set runtimepath+=$HOME/.dotvim]]
 vim.g.python3_host_prog = '/usr/bin/python3'
 
-vim.g.compiled_llvm_clang_directory = '/home/hawtian/project/llvm-project-11.1.0.src/Build'
+vim.g.compiled_llvm_clang_directory = '/home/hawtian/project/llvm-project-12.0.0.src/build'
 
 require('walnut.init')
 

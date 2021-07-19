6cdede4326d102733b963f08f754f6843592fe12
Hawtian Wang <twistoy.wang@gmail.com>
2021-07-19 14:37:15 +0800

    impl py script

--- git-add-and-commit.py
+++ git-add-and-commit.py
@@ -1,12 +1,55 @@
 #!/usr/bin/env python3
 
 import subprocess
+import datetime
+import os
+import sys
+
+def repo_home():
+  p = subprocess.Popen(['git', 'rev-parse', '--show-toplevel'], stdout=subprocess.PIPE)
+  p.wait()
+  return p.stdout.read().decode().strip()
+
 
 def get_uncommit_changes():
   p = subprocess.Popen(['git', 'status', '--porcelain'], stdout=subprocess.PIPE)
   p.wait()
-  return p.stdout.read().decode()
+  return p.stdout.read().decode().strip()
 
-print(get_uncommit_changes())
+
+def update_version():
+  version_template = '''module('walnut.version', package.seeall)
+
+last_updated_time = '{}.{}.{}'
+
+'''
+  now = datetime.datetime.now()
+  version_str = version_template.format(now.year, now.month, now.day)
+  with open(os.path.join(repo_home(), 'lua/walnut/version.lua'), 'w') as fp:
+    fp.write(version_str)
+
+
+if len(sys.argv) <= 1:
+  print('Require commit message!')
+  exit(1)
+
+
+uncommit_changes = get_uncommit_changes().splitlines()
+if not uncommit_changes:
+  print('Working tree is clean...')
+else:
+  now = datetime.datetime.now()
+  print('   Today: {}.{}.{}'.format(now.year, now.month, now.day))
+  print(' Changes:')
+  for c in uncommit_changes:
+    print(c)
+  continue_ask = input('Continue? [y/N] ')
+  if continue_ask != 'y':
+    print('Abort')
+    exit(1)
+  else:
+    update_version()
+    subprocess.run(['git', 'add', '--all'])
+    subprocess.run(['git', 'commit', '-m', sys.argv[1]])
 
 # vim: sw=2 ts=2

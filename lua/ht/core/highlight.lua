6b4257a8579e4651afc4f6c1a0112508c7058af1
Hawtian Wang <twistoy.wang@gmail.com>
2021-08-23 18:14:13 +0800

    highlight helper functions

--- lua/ht/core/highlight.lua
+++ lua/ht/core/highlight.lua
@@ -1,7 +1,45 @@
 module('ht.core.highlight', package.seeall)
 
+local function validate_value(v)
+  if type(v) == 'string' then
+    return v
+  end
+  if type(v) == 'number' then
+    return string.format('#%x', v)
+  end
+  return ''
+end
+
+function CreateGroups(groups, default)
+  for group_name, highlights in pairs(groups) do
+    local options = {}
+    for k, v in pairs(highlights) do
+      table.insert(options, string.format("%s=%s", k, validate_value(v)))
+    end
+
+    vim.cmd(string.format("highlight %s %s %s", default and "default" or "",
+                          group_name, table.concat(options, ' ')))
+  end
+end
+
+function CreateLinks(links)
+  for higroup, link_to in pairs(links) do
+    if type(link_to) == 'string' then
+      vim.cmd(string.format("highlight default link %s %s", higroup, link_to))
+    end
+    if type(link_to) == 'table' then
+      local togroup = link_to[1]
+      if togroup == nil then
+        error("togoup must exists")
+      end
+      vim.cmd(string.format("highlight%s link %s %s",
+                            link_to.force and "!" or " default", higroup,
+                            togroup))
+    end
+  end
+end
+
 function Hi(opts)
-  print(vim.inspect(opts))
   for group_name, definition in pairs(opts) do
     local cmd = 'hi! ' .. group_name
     local count = 0

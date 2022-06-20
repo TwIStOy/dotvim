1816b4db96db005989f1bde6c7276ef8a32cf5a6
Hawtian Wang <twistoy.wang@gmail.com>
2022-06-20 11:42:32 +0800

    fix new diffview config

--- lua/ht/plugs/diffview.lua
+++ lua/ht/plugs/diffview.lua
@@ -8,38 +8,35 @@ function config()
     diff_binaries = false,
     enhanced_diff_hl = true,
     use_icons = true,
-    icons = {
-      folder_closed = "",
-      folder_open = ""
-    },
-    signs = {fold_closed = "", fold_open = ""},
+    icons = { folder_closed = "", folder_open = "" },
+    signs = { fold_closed = "", fold_open = "" },
     file_panel = {
-      position = "left",
-      width = 35,
-      height = 10,
+      win_config = { position = "left", width = 35, height = 10 },
       listing_style = "tree",
-      tree_options = {
-        flatten_dirs = true,
-        folder_statuses = "always"
-      }
+      tree_options = { flatten_dirs = true, folder_statuses = "always" },
     },
     file_history_panel = {
-      position = "bottom",
-      width = 35,
-      height = 16,
+      win_config = { position = "bottom", width = 35, height = 16 },
       log_options = {
-        max_count = 256,
-        follow = false,
-        all = false,
-        merges = false,
-        no_merges = false,
-        reverse = false
-      }
+        single_file = {
+          max_count = 512,
+          follow = true,
+          all = false,
+          merges = false,
+          no_merges = false,
+          reverse = false,
+        },
+        multi_file = {
+          max_count = 128,
+          follow = false,
+          all = false,
+          merges = false,
+          no_merges = false,
+          reverse = false,
+        },
+      },
     },
-    default_args = {
-      DiffviewOpen = {},
-      DiffviewFileHistory = {}
-    },
+    default_args = { DiffviewOpen = {}, DiffviewFileHistory = {} },
     key_bindings = {
       disable_defaults = false, -- Disable the default key bindings
       -- The `view` bindings are active in the diff buffers, only when the current
@@ -51,7 +48,7 @@ function config()
         ["<C-w><C-f>"] = cb("goto_file_split"), -- Open the file in a new split
         ["<C-w>gf"] = cb("goto_file_tab"), -- Open the file in a new tabpage
         ["<leader>e"] = cb("focus_files"), -- Bring focus to the files panel
-        ["<leader>b"] = cb("toggle_files") -- Toggle the files panel.
+        ["<leader>b"] = cb("toggle_files"), -- Toggle the files panel.
       },
       file_panel = {
         ["j"] = cb("next_entry"), -- Bring the cursor to the next file entry
@@ -74,7 +71,7 @@ function config()
         ["i"] = cb("listing_style"), -- Toggle between 'list' and 'tree' views
         ["f"] = cb("toggle_flatten_dirs"), -- Flatten empty subdirectories in tree listing style.
         ["<leader>e"] = cb("focus_files"),
-        ["<leader>b"] = cb("toggle_files")
+        ["<leader>b"] = cb("toggle_files"),
       },
       file_history_panel = {
         ["g!"] = cb("options"), -- Open the option panel
@@ -95,10 +92,10 @@ function config()
         ["<C-w><C-f>"] = cb("goto_file_split"),
         ["<C-w>gf"] = cb("goto_file_tab"),
         ["<leader>e"] = cb("focus_files"),
-        ["<leader>b"] = cb("toggle_files")
+        ["<leader>b"] = cb("toggle_files"),
       },
-      option_panel = {["<tab>"] = cb("select"), ["q"] = cb("close")}
-    }
+      option_panel = { ["<tab>"] = cb("select"), ["q"] = cb("close") },
+    },
   }
 end
 

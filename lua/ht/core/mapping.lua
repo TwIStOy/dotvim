aa72c95d77e7ca30b761f0d4870cbf7afe20f3d9
Hawtian Wang <twistoy.wang@gmail.com>
2022-09-01 18:31:20 +0800

    reimpl mapping

--- lua/ht/core/mapping.lua
+++ lua/ht/core/mapping.lua
@@ -2,45 +2,106 @@ local M = {}
 
 local hut = require 'ht.utils.table'
 
-local filetype_mappings = {
-  mappings = {},
+local filetype_mappings = { ft = '*', mappings = {} }
 
-  _goto = function(self, folder, root)
-    local now = root or self.mappings
-    if #folder == 0 then
-      return now
-    end
+filetype_mappings.new = function(self, ft)
+  local new_obj = vim.deepcopy(self)
+  new_obj.ft = ft
+  return new_obj
+end
 
-    local key = folder[1]
-    if now[key] == nil then
-      now[key] = {}
-    end
+filetype_mappings._goto = function(self, folder, root)
+  local now = root or self.mappings
+  if #folder == 0 then
+    return now
+  end
 
-    return self:_goto(now[key], hut.slice(folder, 2))
-  end,
+  local key = folder[1]
+  if now[key] == nil then
+    now[key] = {}
+  end
 
-  append_folder_name = function(self, folder, name)
-    local folder_table = self:_goto(folder)
-    if folder_table.name ~= nil then
-      table.insert(folder_table.name, name)
-      hut.unique(folder_table)
+  return self:_goto(now[key], hut.slice(folder, 2))
+end
+
+filetype_mappings.append_folder_name = function(self, folder, name)
+  local folder_table = self:_goto(folder)
+  if folder_table.name ~= nil then
+    table.insert(folder_table.name, name)
+    hut.unique(folder_table)
+  else
+    folder_table.name = { name }
+  end
+end
+
+local function create_table(keys, action, desc)
+  local res = { action, desc }
+  if #keys == 0 then
+    return res
+  end
+  res[keys[1]] = res[hut.slice(keys, 2)]
+  return res
+end
+
+filetype_mappings.map = function(self, opt, buffer)
+  if opt.keys == nil or #opt.keys == 0 then
+    error [[invalid 'keys' in opt]]
+  end
+  -- generate wk table
+  local tbl = create_table(opt.keys, opt.action, opt.desc)
+  local mapping_opt = {}
+
+  mapping_opt.mode = opt.mode
+  mapping_opt.silent = opt.silent
+  mapping_opt.noremap = opt.noremap
+  mapping_opt.nowait = opt.nowait
+
+  if self.ft == '*' then
+    mapping_opt.buffer = nil
+    local wk = require("which-key")
+    wk.register(tbl, mapping_opt)
+  else
+    if buffer == nil then
+      -- recording call
+      table.insert(self.mappings, { tbl, mapping_opt })
     else
-      folder_table.name = { name }
+      -- register call
+      mapping_opt.buffer = buffer
+      local wk = require("which-key")
+      wk.register(tbl, mapping_opt)
     end
-  end,
-}
+  end
+end
+
+filetype_mappings.register = function(self, buffer)
+  if self.ft == '*' then
+    return
+  else
+    local wk = require("which-key")
+    for _, v in ipairs(self.mappings) do
+      v[2].buffer = buffer
+      wk.register(v[1], v[2])
+    end
+  end
+end
 
 M.filetypes = {}
 
 M.append_folder_name = function(self, ft, folder, name)
   if self.filetypes[ft] ~= nil then
-    self.filetypes[ft] = vim.deepcopy(filetype_mappings)
+    self.filetypes[ft] = filetype_mappings:new(ft)
   end
 
   self.filetypes[ft]:append_folder_name(folder, name)
 end
 
-M.
+M.map = function(self, ft, opt)
+  if self.filetypes[ft] ~= nil then
+    self.filetypes[ft] = filetype_mappings:new(ft)
+  end
+
+  self.filetypes[ft]:map(opt)
+end
 
 return M
 

768fe5e701c19ce2c816eb9600b904e0e269b7aa
Hawtian Wang <twistoy.wang@gmail.com>
2022-09-02 17:36:18 +0800

    fix

--- lua/ht/plugins/lualine.lua
+++ lua/ht/plugins/lualine.lua
@@ -9,31 +9,31 @@ M.core = {
 M.setup = function() -- code to run before plugin loaded
 end
 
-local options = {
-  section_separators = { '', '' },
-  component_separators = { '', '' },
-  theme = 'auto',
-}
-
-local sections = {
-  lualine_a = { 'mode' },
-  lualine_b = { coc_status, 'branch', 'diff' },
-  lualine_c = { 'filename' },
-  lualine_x = { 'encoding', 'fileformat', 'filetype' },
-  lualine_y = { 'progress' },
-  lualine_z = { 'location' },
-}
-
-local inactive_sections = {
-  lualine_a = {},
-  lualine_b = {},
-  lualine_c = { 'filename' },
-  lualine_x = { 'location' },
-  lualine_y = {},
-  lualine_z = {},
-}
-
 M.config = function() -- code to run after plugin loaded
+  local options = {
+    section_separators = { '', '' },
+    component_separators = { '', '' },
+    theme = 'auto',
+  }
+  
+  local sections = {
+    lualine_a = { 'mode' },
+    lualine_b = { coc_status, 'branch', 'diff' },
+    lualine_c = { 'filename' },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' },
+  }
+  
+  local inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { 'filename' },
+    lualine_x = { 'location' },
+    lualine_y = {},
+    lualine_z = {},
+  }
+    
   require'lualine'.setup {
     options = options,
     sections = sections,

54c7dfbd5cdf78278713e66d4ac983c444769ee6
Hawtian Wang <twistoy.wang@gmail.com>
2022-09-02 14:41:31 +0800

    remove old

---
+++ lua/ht/plugins/tcomment.lua
@@ -0,0 +1,35 @@
+local M = {}
+
+M.core = { 'tomtom/tcomment_vim', opt = true }
+
+M.setup = function() -- code to run before plugin loaded
+  vim.g.tcomment_maps = 0
+  vim.defer_fn(function()
+    require'packer'.loader('tcomment_vim')
+  end, 700)
+end
+
+M.config = function() -- code to run after plugin loaded
+end
+
+M.mappings = function() -- code for mappings
+  local mapping = require 'ht.core.mapping'
+
+  mapping.map({
+    keys = { 'g', 'c', 'c' },
+    action = '<cmd>TComment<CR>',
+    desc = 'toggle-comment',
+  })
+
+  mapping.map({
+    mode = 'v',
+    keys = { 'g', 'c', 'c' },
+    action = '<cmd>TCommentBlock<CR>',
+    desc = 'toggle-comment',
+  })
+end
+
+return M
+
+-- vim: et sw=2 ts=2 fdm=marker
+
--- lua/ht/plugs/.vim-template:.lua
+++
@@ -1,6 +0,0 @@
-module('ht.plugs.%FILE%', package.seeall)
-
-%HERE%
-
--- vim: et sw=2 ts=2
-

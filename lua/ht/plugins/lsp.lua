702af895ac2777acb99c8beb9f1c1cee5be4f295
Hawtian Wang <twistoy.wang@gmail.com>
2022-10-16 14:49:27 +0800

    replace deprecated functions

--- lua/ht/plugins/lsp.lua
+++ lua/ht/plugins/lsp.lua
@@ -129,9 +129,9 @@ end
 M.config = function() -- code to run after plugin loaded
   require'fidget'.setup {}
 
-  local capabilities = require'cmp_nvim_lsp'.update_capabilities(vim.lsp
-                                                                     .protocol
-                                                                     .make_client_capabilities())
+  local capabilities = require'cmp_nvim_lsp'.default_capabilities(vim.lsp
+                                                                      .protocol
+                                                                      .make_client_capabilities())
 
   local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
   for type, icon in pairs(signs) do

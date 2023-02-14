cfcfd509dd561b8986d346f2952a025ede045d10
Hawtian Wang <twistoy.wang@gmail.com>
2023-02-14 23:42:30 +0800

    Fix

--- lua/ht/lazy.lua
+++ lua/ht/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("lazy_plugs", {
+require("lazy").setup("ht.plugrc", {
   performance = {
     rtp = {
       paths = {

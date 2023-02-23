f0e697ade82d5ee754f4ccb4b7856f37fe9ff5c2
Hawtian Wang <twistoy.wang@gmail.com>
2023-02-23 19:32:46 +0800

    Configure BufferLine for multiple windows and highlights
    - Set up BufferLine with options to view multiple windows, set highlights, and add mappings
    - Move mappings for navigating between windows
    - Add configuration function for BufferLine
    
    [lua/ht/plugrc/bufferline.lua]
    - Set up BufferLine with options to view multiple windows, set highlights, and add mappings
    - Move `BufferLineMovePrev` and `BufferLineMoveNext` mappings
    - Add configuration function for BufferLine

--- lua/ht/plugrc/bufferline.lua
+++ lua/ht/plugrc/bufferline.lua
@@ -10,27 +10,29 @@ return {
       { '<M-<>', '<cmd>BufferLineMovePrev<CR>' },
       { '<M->>', '<cmd>BufferLineMoveNext<CR>' },
     },
-    opts = {
-      options = {
-        view = 'multiwindow',
-        highlights = require("catppuccin.groups.integrations.bufferline").get(),
-        hover = { enabled = true, delay = 200 },
-        separator_style = 'thin',
-        close_command = "Bdelete! %d",
-        numbers = function(opts)
-          return string.format('%s¬∑%s', opts.raise(opts.id),
-                               opts.lower(opts.ordinal))
-        end,
-        diagnostics = 'nvim_lsp',
-        offsets = {
-          {
-            filetype = "NvimTree",
-            text = "File Explorer",
-            text_align = "center",
-            highlight = 'Directory',
+    config = function()
+      require'bufferline'.setup {
+        options = {
+          view = 'multiwindow',
+          highlights = require("catppuccin.groups.integrations.bufferline").get(),
+          hover = { enabled = true, delay = 200 },
+          separator_style = 'thin',
+          close_command = "Bdelete! %d",
+          numbers = function(opts)
+            return string.format('%s¬∑%s', opts.raise(opts.id),
+                                 opts.lower(opts.ordinal))
+          end,
+          diagnostics = 'nvim_lsp',
+          offsets = {
+            {
+              filetype = "NvimTree",
+              text = "File Explorer",
+              text_align = "center",
+              highlight = 'Directory',
+            },
           },
         },
-      },
-    },
+      }
+    end,
   },
 }

5abc1da391ba796a45869bb8d4b128ddd0101f89
Hawtian Wang <twistoy.wang@gmail.com>
2023-04-11 21:55:18 +0800

    fix: remove deprecated options

--- lua/ht/plugrc/nvim-tree.lua
+++ lua/ht/plugrc/nvim-tree.lua
@@ -22,8 +22,6 @@ M.config = function() -- code to run after plugin loaded
   require'nvim-tree'.setup {
     disable_netrw = true,
     hijack_netrw = true,
-    open_on_setup = false,
-    ignore_ft_on_setup = {},
     open_on_tab = false,
     hijack_cursor = false,
     update_cwd = false,

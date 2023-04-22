a8451e4f6f95d41df117c20f6cdbfb9bea5a8053
Hawtian Wang <twistoy.wang@gmail.com>
2023-04-22 11:27:31 +0800

    fix: update keymaps for cpp-toolkit

--- lua/ht/plugrc/lang/cpp.lua
+++ lua/ht/plugrc/lang/cpp.lua
@@ -4,16 +4,32 @@ return {
     config = function()
       require'cpp-toolkit'.setup()
     end,
-    cmd = { 'CppGenDef', 'CppDebugPrint' },
+    cmd = { 'CppGenDef', 'CppDebugPrint', 'CppToolkit', 'Telescope' },
     dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
     keys = {
       {
-        '<C-e><C-i>',
+        '<C-f>i',
         function()
           vim.cmd [[Telescope cpptoolkit insert_header]]
         end,
         desc = 'insert-header',
         mode = { 'i', 'n' },
+      },
+      {
+        '<C-f>m',
+        function()
+          require'cpp-toolkit.functions.shortcut'.shortcut_move_value()
+        end,
+        desc = 'move-value',
+        mode = { 'n' },
+      },
+      {
+        '<C-f>f',
+        function()
+          require'cpp-toolkit.functions.shortcut'.shortcut_forward_value()
+        end,
+        desc = 'forward-value',
+        mode = { 'n' },
       },
     },
   },

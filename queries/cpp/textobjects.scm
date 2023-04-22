6e78cd7862aae447381f4a431db6b0fc5e380c8f
Hawtian Wang <twistoy.wang@gmail.com>
2023-04-22 11:55:31 +0800

    feat: add new textobject assighment.lhs(rhs) for cpp

---
+++ queries/cpp/textobjects.scm
@@ -0,0 +1,9 @@
+; extends
+
+((init_declarator
+  declarator: (_) @assignment.lhs
+  value: (_) @assignment.rhs))
+
+((assignment_expression
+  left: (_) @assignment.lhs
+  right: (_) @assignment.rhs))

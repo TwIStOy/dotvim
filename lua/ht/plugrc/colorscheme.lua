6f713c2891fb1b52b1dd38548611a31f603b4f21
Hawtian Wang <twistoy.wang@gmail.com>
2023-05-06 15:15:15 +0800

    feat: update

--- lua/ht/plugrc/colorscheme.lua
+++ lua/ht/plugrc/colorscheme.lua
@@ -257,7 +257,7 @@ return {
     build = ":CatppuccinCompile",
     config = function()
       require("catppuccin").setup {
-        flavour = "macchiato",
+        flavour = "mocha",
         term_colors = false,
         transparent_background = false,
         compile_path = vim.fn.stdpath("cache") .. "/catppuccin",

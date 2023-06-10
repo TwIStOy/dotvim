2bbd7b3ccdeb49520c8582864a1244b9715f1dee
Hawtian Wang <twistoy.wang@gmail.com>
2023-06-10 14:02:43 +0800

    fix: fix version template

--- git-add-and-commit.py
+++ git-add-and-commit.py
@@ -20,7 +20,7 @@ def get_uncommit_changes():
 def update_version():
   version_template = '''local M = {{}}
 
-M = last_updated_time = '{}.{:02d}.{:02d}'
+M.last_updated_time = '{}.{:02d}.{:02d}'
 
 return M
 '''
--- lua/ht/version.lua
+++ lua/ht/version.lua
@@ -1,5 +1,5 @@
 local M = {}
 
-M = last_updated_time = '2023.06.10'
+M.last_updated_time = '2023.06.10'
 
 return M

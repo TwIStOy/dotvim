df1ea7c79d4b88576b5fcdc1dbe42d31afbe1540
Hawtian Wang <twistoy.wang@gmail.com>
2023-06-18 20:15:52 +0800

    try another theme

--- lua/ht/plugrc/colorscheme.lua
+++ lua/ht/plugrc/colorscheme.lua
@@ -165,8 +165,38 @@ return {
   },
 
   {
-    "Alexis12119/nightly.nvim",
+    "rose-pine/neovim",
+    name = "rose-pine",
     enabled = true,
+    lazy = false,
+    config = function()
+      require("rose-pine").setup {
+        variant = "moon",
+        highlight_groups = {
+          TelescopeBorder = { fg = "highlight_high", bg = "none" },
+          TelescopeNormal = { bg = "none" },
+          TelescopePromptNormal = { bg = "base" },
+          TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+          TelescopeSelection = { fg = "text", bg = "base" },
+          TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
+
+          DiagnosticUnderlineWarn = {
+            sp = "gold",
+            undercurl = true,
+          },
+          DiagnosticUnderlineError = {
+            sp = "love",
+            undercurl = true,
+          },
+        },
+      }
+      vim.cmd("colorscheme rose-pine")
+    end,
+  },
+
+  {
+    "Alexis12119/nightly.nvim",
+    enabled = false,
     lazy = false,
     config = function()
       local p = require("nightly.palette").dark_colors

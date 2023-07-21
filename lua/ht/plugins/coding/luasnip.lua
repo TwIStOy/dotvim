f364b5a6f8190b61064d37a6aad171b99cbfbe50
Hawtian Wang <twistoy.wang@gmail.com>
2023-07-21 11:16:20 +0800

    feat: update deps

--- lua/ht/plugins/coding/luasnip.lua
+++ lua/ht/plugins/coding/luasnip.lua
@@ -18,6 +18,23 @@ return {
       luasnip.add_snippets("cpp", require("ht.snippets.cpp")())
       luasnip.add_snippets("rust", require("ht.snippets.rust")())
       luasnip.add_snippets("lua", require("ht.snippets.lua")())
+
+      -- from: https://github.com/L3MON4D3/LuaSnip/issues/258
+      vim.api.nvim_create_autocmd("ModeChanged", {
+        pattern = "*",
+        callback = function()
+          if
+            (
+              (vim.v.event.old_mode == "s" and vim.v.event.new_mode == "n")
+              or vim.v.event.old_mode == "i"
+            )
+            and require("luasnip").session.current_nodes[vim.api.nvim_get_current_buf()]
+            and not require("luasnip").session.jump_active
+          then
+            require("luasnip").unlink_current()
+          end
+        end,
+      })
     end,
     keys = {
       {

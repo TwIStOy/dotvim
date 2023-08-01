46755a5e3abaf04a84b1f5a53e9ff146f64d9c4c
Hawtian Wang <twistoy.wang@gmail.com>
2023-08-01 17:25:09 +0800

    feat: in_git_repo can pass a VimBuffer to check its parent path

--- lua/ht/utils/fs.lua
+++ lua/ht/utils/fs.lua
@@ -68,13 +68,22 @@ function M.topmost_parent(p, matcher)
 end
 
 ---Check if the current directory is in a git repo.
+---@param buffer VimBuffer?
 ---@return boolean
-function M.in_git_repo()
-  local p = vim.system {
+function M.in_git_repo(buffer)
+  local Path = require("plenary.path")
+  local cwd = nil
+  if buffer ~= nil and buffer.filename ~= nil and buffer.filename ~= "" then
+    cwd = tostring(Path.new(buffer.filename):parent())
+  end
+
+  local p = vim.system({
     "git",
     "rev-parse",
     "--is-inside-work-tree",
-  }
+  }, {
+    cwd = cwd,
+  })
   local res = p:wait()
   return res.code == 0
 end

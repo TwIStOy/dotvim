1891ff4ea7153f1b25cc7a7597f3c44b6afaca94
Hawtian Wang <twistoy.wang@gmail.com>
2023-08-01 23:10:16 +0800

    feat: add extra cpp textobjects

--- queries/cpp/textobjects.scm
+++ queries/cpp/textobjects.scm
@@ -1,9 +1,29 @@
 ; extends
 
-; ((init_declarator
-;   declarator: (_) @assignment.lhs
-;   value: (_) @assignment.rhs))
-;
-; ((assignment_expression
-;   left: (_) @assignment.lhs
-;   right: (_) @assignment.rhs))
+((init_declarator
+   declarator: (_) @assignment.lhs
+   value: (_) @assignment.rhs))
+
+((assignment_expression
+   left: (_) @assignment.lhs
+   right: (_) @assignment.rhs))
+
+((initializer_pair
+   designator: (_) @assignment.lhs
+   value: (_) @assignment.rhs))
+
+(lambda_expression
+  body:  (compound_statement)) @function.outer
+
+(lambda_expression
+  body: (compound_statement . "{" . (_) @_start @_end (_)? @_end . "}"
+ (#make-range! "function.inner" @_start @_end)))
+
+((structured_binding_declarator
+   "," @_start . (_) @parameter.inner @_end)
+ (#make-range! "parameter.outer" @_start @parameter.inner))
+((structured_binding_declarator
+   . (_) @parameter.inner . ","? @_end)
+  (#make-range! "parameter.outer" @parameter.inner @_end))
+  
+

2f2c968910334b873e2aa2ce06add79efe095fb3
Hawtian Wang <twistoy.wang@gmail.com>
2023-08-01 18:13:44 +0800

    feat: add TSPlayground into command plate

--- lua/ht/plugins/tree-sitter/playground.lua
+++ lua/ht/plugins/tree-sitter/playground.lua
@@ -1,8 +1,23 @@
 return {
-  {
+  Use {
     "nvim-treesitter/playground",
-    lazy = true,
-    dependencies = { "nvim-treesitter/nvim-treesitter" },
-    cmd = { "TSPlaygroundToggle", "TSHighlightCapturesUnderCursor" },
+    lazy = {
+      lazy = true,
+      dependencies = { "nvim-treesitter/nvim-treesitter" },
+      cmd = { "TSPlaygroundToggle", "TSHighlightCapturesUnderCursor" },
+    },
+    functions = {
+      {
+        filter = {
+          ---@param buffer VimBuffer
+          filter = function(buffer)
+            return #buffer.ts_highlights > 0
+          end,
+        },
+        values = {
+          FuncSpec("Toggle tree-sitter playground", "TSPlaygroundToggle"),
+        },
+      },
+    },
   },
 }

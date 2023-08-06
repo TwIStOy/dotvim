174d80c3048d28490bac6bd7704062ceb51c82a1
Hawtian Wang <twistoy.wang@gmail.com>
2023-08-06 22:14:49 +0800

    fix: invoke `attach_to_buffer` manually when nvim-colorizer first loaded

--- lua/ht/plugins/ui/nvim-colorizer.lua
+++ lua/ht/plugins/ui/nvim-colorizer.lua
@@ -6,15 +6,30 @@ return {
       "vim",
       "lua",
     },
+    cmd = {
+      "ColorizerAttachToBuffer",
+      "ColorizerDetachFromBuffer",
+      "ColorizerReloadAllBuffers",
+      "ColorizerToggle",
+    },
     opts = {
       filetypes = {
         "vim",
         "lua",
       },
       user_default_options = {
+        RRGGBB = true,
         names = false,
       },
     },
-    config = true,
+    config = function(_, opts)
+      require("colorizer").setup(opts)
+      local ft = vim.api.nvim_get_option_value("filetype", {
+        buf = 0,
+      })
+      if ft == "vim" or ft == "lua" then
+        require("colorizer").attach_to_buffer(0)
+      end
+    end,
   },
 }

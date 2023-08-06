cdd8e09d4202940373d0dbc197ab7c9581b354f3
Hawtian Wang <twistoy.wang@gmail.com>
2023-08-06 23:30:33 +0800

    feat: remove old postfix impl

--- lua/ht/snippets/cpp/ts_postfix.lua
+++ lua/ht/snippets/cpp/ts_postfix.lua
@@ -1,33 +1,8 @@
 local ht_snippet = require("ht.snippets.snippet")
 local snippet = ht_snippet.build_snippet
-local trig_engines = require("ht.snippets.trig_engines")
-local events = require("luasnip.util.events")
 local ts_resolver = require("ht.snippets.resolvers.ts_resolver")
 
 local function ts_postfix_maker(types)
-  return function(opts)
-    opts.engine = trig_engines.ts_topmost_parent(types)
-    opts.hidden = true
-    opts.opts = {
-      callbacks = {
-        [-1] = {
-          ---@diagnostic disable-next-line: unused-local
-          [events.pre_expand] = function(node, _event_args)
-            local postfix_env_override = {
-              env_override = {
-                POSTFIX_MATCH = node.captures[1],
-              },
-            }
-            return postfix_env_override
-          end,
-        },
-      },
-    }
-    return snippet(opts)
-  end
-end
-
-local function new_ts_postfix_maker(types)
   return function(opts)
     opts.engine = "plain"
     opts.hidden = true
@@ -52,9 +27,8 @@ local indent_only_types = {
 
 return {
   ts_postfix_maker = ts_postfix_maker,
-  new_ts_postfix_maker = new_ts_postfix_maker,
   any_expr_types = any_expr_types,
   indent_only_types = indent_only_types,
-  cpp_ts_postfix_any_expr = new_ts_postfix_maker(any_expr_types),
-  cpp_ts_postfix_ident_only = new_ts_postfix_maker(indent_only_types),
+  cpp_ts_postfix_any_expr = ts_postfix_maker(any_expr_types),
+  cpp_ts_postfix_ident_only = ts_postfix_maker(indent_only_types),
 }

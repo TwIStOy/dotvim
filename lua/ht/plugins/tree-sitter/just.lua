24f43853ed9237e119fa4c9892e4a61ba24e88b6
Hawtian Wang <twistoy.wang@gmail.com>
2023-08-31 20:19:13 +0800

    feat: add justfile support

---
+++ lua/ht/plugins/tree-sitter/just.lua
@@ -0,0 +1,14 @@
+return {
+  "IndianBoy42/tree-sitter-just",
+  config = function()
+    require("nvim-treesitter.parsers").get_parser_configs().just = {
+      install_info = {
+        url = "https://github.com/IndianBoy42/tree-sitter-just", -- local path or git repo
+        files = { "src/parser.c", "src/scanner.cc" },
+        branch = "main",
+        use_makefile = true, -- this may be necessary on MacOS (try if you see compiler errors)
+      },
+      maintainers = { "@IndianBoy42" },
+    }
+  end,
+}

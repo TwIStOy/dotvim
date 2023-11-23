2594664b25aa92112ed0d5948c40af99addae3c6
Hawtian <twistoy.wang@gmail.com>
2023-11-23 11:29:27 +0800

    refactor: move lib

--- src/conf/plugins/lib/index.ts
+++ src/conf/plugins/lib/index.ts
@@ -1,3 +1,20 @@
+import { Plugin } from "@core/model";
 import { plugin as imageNvim } from "./image-nvim";
 
-export const plugins = [imageNvim] as const;
+
+function newLibPlugin(name: string) {
+  return new Plugin({
+    shortUrl: name,
+    lazy: {
+      lazy: true,
+    }
+  });
+}
+
+export const plugins = [
+  newLibPlugin("MunifTanjim/nui.nvim"),
+  newLibPlugin("nvim-lua/popup.nvim"),
+  newLibPlugin("nvim-lua/plenary.nvim"),
+  newLibPlugin("nvim-tree/nvim-web-devicons"),
+  imageNvim
+] as const;

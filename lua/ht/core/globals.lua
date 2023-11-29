039041ea919644aed821c4af5efab3cb4f494cc0
Hawtian Wang <twistoy.wang@gmail.com>
2023-11-29 21:30:18 +0800

    feat: try two path

--- lua/ht/core/globals.lua
+++ lua/ht/core/globals.lua
@@ -1,12 +1,21 @@
 local M = {}
 
-local default_vault_path = "~/Documents/Main"
+local default_vault_path = {
+  "~/Documents/Main",
+  "~/Projects/obsidian-data/Main",
+}
 
 local function resolve_obsidian_vault()
   if vim.g.obsidian_vault_path then
     return vim.fn.resolve(vim.fn.expand(vim.g.obsidian_vault_path))
   end
-  return vim.fn.resolve(vim.fn.expand(default_vault_path))
+  for _, path in ipairs(default_vault_path) do
+    local p = vim.fn.resolve(vim.fn.expand(path))
+    if vim.fn.isdirectory(p) == 1 then
+      return p
+    end
+  end
+  return vim.fn.resolve(vim.fn.expand(default_vault_path[1]))
 end
 
 local function resolve_has_obsidian_vault()

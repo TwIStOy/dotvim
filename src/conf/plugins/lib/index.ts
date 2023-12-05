5d5f238360aa1d2aeb8580b27ade9017b45d72f8
Hawtian Wang <twistoy.wang@gmail.com>
2023-12-05 18:55:22 +0800

    try kui

--- src/conf/plugins/lib/index.ts
+++ src/conf/plugins/lib/index.ts
@@ -1,13 +1,13 @@
 import { Plugin } from "@core/model";
 import { plugin as imageNvim } from "./image-nvim";
-
+import { plugin as kuinNvim } from "./kui-nvim";
 
 function newLibPlugin(name: string) {
   return new Plugin({
     shortUrl: name,
     lazy: {
       lazy: true,
-    }
+    },
   });
 }
 
@@ -16,5 +16,6 @@ export const plugins = [
   newLibPlugin("nvim-lua/popup.nvim"),
   newLibPlugin("nvim-lua/plenary.nvim"),
   newLibPlugin("nvim-tree/nvim-web-devicons"),
-  imageNvim
+  imageNvim,
+  kuinNvim,
 ] as const;

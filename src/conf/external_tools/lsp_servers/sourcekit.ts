ef2303a3a8be4c905669fb6b538ab626ac9eb443
Hawtian Wang <hawtian.wang@twistoy.cn>
2024-02-18 11:13:57 +0800

    fix

--- src/conf/external_tools/lsp_servers/sourcekit.ts
+++ src/conf/external_tools/lsp_servers/sourcekit.ts
@@ -9,6 +9,7 @@ export const server = new LspServer({
     capabilities: LuaTable
   ) => {
     luaRequire("lspconfig").sourcekit.setup({
+      cmd: ["xcrun", "--toolchain", "swift", "sourcekit-lsp"],
       filetypes: ["swift", "objective-c", "objective-cpp"],
       on_attach: on_attach,
       capabilities: capabilities,

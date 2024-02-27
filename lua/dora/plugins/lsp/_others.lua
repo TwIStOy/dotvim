6f71aad804ca576365b99d8f3306f81c1bcc5016
Hawtian Wang <twistoy.wang@gmail.com>
2024-02-27 16:54:57 +0800

    feat:

---
+++ lua/dora/plugins/lsp/_others.lua
@@ -0,0 +1,24 @@
+---@type dora.core.plugin.PluginOptions[]
+return {
+  {
+    "nvimdev/lspsaga.nvim",
+    event = "LspAttach",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      code_action = {
+        num_shortcut = true,
+        show_server_name = true,
+        extend_gitsigns = false,
+        keys = { quit = { "q", "<Esc>" }, exec = "<CR>" },
+      },
+      lightbulb = { enable = false },
+      symbol_in_winbar = { enable = false },
+      ui = {
+        border = "none",
+      },
+    },
+  },
+}
--- lua/dora/plugins/lsp/init.lua
+++ lua/dora/plugins/lsp/init.lua
@@ -2,4 +2,5 @@
 local lib = require("dora.lib")
 return lib.tbl.flatten_array {
   require("dora.plugins.lsp.nvim-lspconfig"),
+  require("dora.plugins.lsp._others"),
 }

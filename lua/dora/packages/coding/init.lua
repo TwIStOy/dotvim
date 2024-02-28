2e29f3bd19a7b4a41efda5de1445eea90942d943
Hawtian Wang <twistoy.wang@gmail.com>
2024-02-28 12:07:05 +0800

    ...

--- lua/dora/packages/coding/init.lua
+++ lua/dora/packages/coding/init.lua
@@ -7,6 +7,7 @@ return {
   plugins = lib.tbl.flatten_array {
     require("dora.packages.coding.plugins.nvim-cmp"),
     require("dora.packages.coding.plugins.luasnip"),
+    require("dora.packages.coding.plugins.conform"),
     require("dora.packages.coding.plugins.ultimate-autopair"),
     require("dora.packages.coding.plugins.nvim-surround"),
     require("dora.packages.coding.plugins.nvim-ts-context-commentstring"),
---
+++ lua/dora/packages/coding/plugins/conform.lua
@@ -0,0 +1,30 @@
+---@type dora.core.plugin.PluginOption
+return {
+  "stevearc/conform.nvim",
+  gui = "all",
+  opts = {
+    format = {
+      timeout_ms = 3000,
+      async = false,
+      quiet = false,
+    },
+    formatters = {},
+    formatters_by_ft = {},
+  },
+  cmd = { "Conform" },
+  event = { "BufReadPost" },
+  keys = {
+    {
+      "<leader>cf",
+      function()
+        local conform = require("conform")
+        conform.format {
+          async = true,
+          lsp_fallback = "always",
+        }
+      end,
+      desc = "format-file",
+      mode = { "n", "v" },
+    },
+  },
+}

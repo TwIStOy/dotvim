3cd6c0cdaec0a3370481174569842001f8150363
Hawtian Wang <twistoy.wang@gmail.com>
2024-03-05 17:42:08 +0800

    ...

--- lua/dotvim/packages/_.lua
+++ lua/dotvim/packages/_.lua
@@ -8,9 +8,10 @@ return {
     {
       "catppuccin",
       opts = function(_, opts)
-        opts.flavour = "latte"
+        -- opts.flavour = "latte"
+        opts.flavour = "macchiato"
         opts.color_overrides = {
-          latte = {
+          macchiato = {
             rosewater = "#cc7983",
             flamingo = "#bb5d60",
             pink = "#d54597",
@@ -37,34 +38,6 @@ return {
             base = "#ffffff",
             mantle = "#eeeeee",
             crust = "#dddddd",
-          },
-          macchiato = {
-            rosewater = "#F5B8AB",
-            flamingo = "#F29D9D",
-            pink = "#AD6FF7",
-            mauve = "#FF8F40",
-            red = "#E66767",
-            maroon = "#EB788B",
-            peach = "#FAB770",
-            yellow = "#FACA64",
-            green = "#70CF67",
-            teal = "#4CD4BD",
-            sky = "#61BDFF",
-            sapphire = "#4BA8FA",
-            blue = "#00BFFF",
-            lavender = "#00BBCC",
-            text = "#C1C9E6",
-            subtext1 = "#A3AAC2",
-            subtext0 = "#8E94AB",
-            overlay2 = "#7D8296",
-            overlay1 = "#676B80",
-            overlay0 = "#464957",
-            surface2 = "#3A3D4A",
-            surface1 = "#2F313D",
-            surface0 = "#1D1E29",
-            base = "#0b0b12",
-            mantle = "#11111a",
-            crust = "#191926",
           },
         }
       end,

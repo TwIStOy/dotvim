cebcb86e83b875a2681f1f0e326fc5cc4a866f12
Hawtian Wang <hawtian.wang@twistoy.cn>
2024-03-07 11:30:18 +0800

    feat: fix neovide paste

--- lua/dotvim/packages/_/neovide.lua
+++ lua/dotvim/packages/_/neovide.lua
@@ -64,11 +64,14 @@ function M.setup()
   })
 
   -- clipboard
-  vim.keymap.set("v", "<D-c>", '"+y') -- Copy
-  vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
-  vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
-  vim.keymap.set("c", "<D-v>", "<C-R>+") -- Paste command mode
-  vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
+  vim.keymap.set({ "n", "i", "c", "v" }, "<D-v>", function()
+    local register_info = vim.fn.getreginfo("+")
+    local content = {}
+    if register_info.regcontents then
+      content = register_info.regcontents
+    end
+    vim.api.nvim_paste(table.concat(content, "\n"), true, -1)
+  end, { silent = true })
 end
 
 return M

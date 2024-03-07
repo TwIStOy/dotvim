0a4b0f61d1ff4b18e2a202bed10bd757f0fdf937
Hawtian Wang <hawtian.wang@twistoy.cn>
2024-03-07 14:34:54 +0800

    feat: update neovide options

--- lua/dotvim/packages/_/neovide.lua
+++ lua/dotvim/packages/_/neovide.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-local function set_ime(args)
+local function toggle_ime(args)
   if args.event:match("Enter$") then
     vim.g.neovide_input_ime = true
   else
@@ -23,7 +23,7 @@ function M.setup()
   vim.g.neovide_no_idle = true
   vim.g.neovide_input_macos_alt_is_meta = true
 
-  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_cursor_animation_length = 0.2
   vim.g.neovide_cursor_trail_size = 0
 
   vim.g.neovide_floating_blur_amount_x = 6.0
@@ -39,13 +39,8 @@ function M.setup()
   vim.g.neovide_underline_automatic_scaling = true
   vim.g.neovide_underline_stroke_scale = 0.6
 
-  vim.g.neovide_native_border_width = 1.0
-  vim.g.neovide_native_border_inactive_color = 0xffff0000
-  vim.g.neovide_native_border_active_color = 0xff477fef
-  vim.g.neovide_unlink_border_highlights = true
-
-  vim.o.winblend = 20
-  vim.o.pumblend = 20
+  vim.o.winblend = 30
+  vim.o.pumblend = 30
 
   vim.g.neovide_input_ime = false
 
@@ -54,13 +49,13 @@ function M.setup()
   vim.api.nvim_create_autocmd({ "InsertEnter", "InsertLeave" }, {
     group = ime_input,
     pattern = "*",
-    callback = set_ime,
+    callback = toggle_ime,
   })
 
   vim.api.nvim_create_autocmd({ "CmdlineEnter", "CmdlineLeave" }, {
     group = ime_input,
     pattern = "[/\\?]",
-    callback = set_ime,
+    callback = toggle_ime,
   })
 
   -- clipboard

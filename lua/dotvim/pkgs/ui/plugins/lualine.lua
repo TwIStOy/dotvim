6d13b446a1f9f94e6c3eb869dc414163e1cabfcf
Hawtian Wang <hawtian.wang@twistoy.cn>
2024-05-06 10:31:12 +0800

    ...

--- lua/dotvim/pkgs/ui/plugins/lualine.lua
+++ lua/dotvim/pkgs/ui/plugins/lualine.lua
@@ -51,8 +51,8 @@ return {
     return {
       options = {
         component_separators = { left = "|", right = "|" },
-        -- section_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        -- section_separators = { left = "", right = "" },
         theme = "auto",
         globalstatus = true,
         disabled_filetypes = {

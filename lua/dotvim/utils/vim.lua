ae88592153ca08b6754986681193933b841fb51f
Hawtian Wang <twistoy.wang@gmail.com>
2024-05-08 17:46:29 +0800

    ...

--- lua/dotvim/utils/vim.lua
+++ lua/dotvim/utils/vim.lua
@@ -3,10 +3,12 @@ local M = {}
 
 function M.cursor0_to_editor()
   local win = vim.api.nvim_get_current_win()
-  local win_row, win_col = unpack(vim.api.nvim_win_get_position(win))
-  local row, col = unpack(vim.api.nvim_win_get_cursor(win))
+  local win_row = unpack(vim.api.nvim_win_get_position(win))
+  local row = unpack(vim.api.nvim_win_get_cursor(win))
   local line_start = vim.api.nvim_call_function("line", { "w0" })
-  return win_row + row - line_start, win_col + col
+  local col = vim.fn.screencol()
+  print(row, col)
+  return win_row + row - line_start, col
 end
 
 return M

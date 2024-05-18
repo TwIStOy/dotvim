1565b74a2a75a03a32f7a9f45d60c6ae8e172d56
Hawtian Wang <twistoy.wang@gmail.com>
2024-05-18 12:48:13 +0800

    ...

--- lua/dotvim/pkgs/base/setup/neovide.lua
+++ lua/dotvim/pkgs/base/setup/neovide.lua
@@ -21,7 +21,7 @@ function M.setup()
   vim.g.neovide_profiler = false
   vim.g.neovide_refresh_rate = 60
   vim.g.neovide_no_idle = true
-  vim.g.neovide_input_macos_option_key_is_meta = true
+  vim.g.neovide_input_macos_option_key_is_meta = "only_left"
 
   vim.g.neovide_cursor_animation_length = 0.2
   vim.g.neovide_cursor_trail_size = 0

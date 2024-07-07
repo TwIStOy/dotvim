5b229d869587076a07ca2a75c54156e287838b1a
Hawtian Wang <twistoy.wang@gmail.com>
2024-07-07 12:53:50 +0800

    feat: update tokyonight

--- lua/dotvim/pkgs/theme/plugins/tokyo-night.lua
+++ lua/dotvim/pkgs/theme/plugins/tokyo-night.lua
@@ -14,12 +14,42 @@ return {
     transparent = false,
     day_brightness = 0.3,
     sidebars = { "qf", "vista_kind", "terminal", "packer", "help" },
-    on_highlights = function(highlights, colors)
-      highlights["@lsp.typemod.variable.mutable.rust"] = {
+    on_highlights = function(hl, c)
+      hl["@lsp.typemod.variable.mutable.rust"] = {
         underline = true,
       }
-      highlights["@lsp.typemod.selfKeyword.mutable.rust"] = {
+      hl["@lsp.typemod.selfKeyword.mutable.rust"] = {
         underline = true,
+      }
+
+      -- borderless telescope
+      local prompt = "#2d3149"
+      hl.TelescopeNormal = {
+        bg = c.bg_dark,
+        fg = c.fg_dark,
+      }
+      hl.TelescopeBorder = {
+        bg = c.bg_dark,
+        fg = c.bg_dark,
+      }
+      hl.TelescopePromptNormal = {
+        bg = prompt,
+      }
+      hl.TelescopePromptBorder = {
+        bg = prompt,
+        fg = prompt,
+      }
+      hl.TelescopePromptTitle = {
+        bg = prompt,
+        fg = prompt,
+      }
+      hl.TelescopePreviewTitle = {
+        bg = c.bg_dark,
+        fg = c.bg_dark,
+      }
+      hl.TelescopeResultsTitle = {
+        bg = c.bg_dark,
+        fg = c.bg_dark,
       }
     end,
   },

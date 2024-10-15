a271b1084ecbaf81f9b200eea245d2d8fa63f125
Hawtian Wang <twistoy.wang@gmail.com>
2024-10-15 23:10:51 +0800

    feat: better separator

--- lua/dotvim/extra/context_menu.lua
+++ lua/dotvim/extra/context_menu.lua
@@ -10,17 +10,29 @@ local Utils = require("dotvim.extra.context_menus.utils")
 ---@return dotvim.extra.ContextMenuOptions
 local function build_nodes()
   return {
-    Groups.build_plugin_refactor(),
-    { name = "separator" },
-    Groups.build_plugin_rust_lsp(),
+    {
+      Groups.build_plugin_refactor(),
+    },
+    {
+      Groups.build_plugin_rust_lsp(),
+    },
   }
 end
 
 M.open_context_menu = function()
   local nodes = {}
   for _, node in ipairs(build_nodes()) do
-    if Utils.option_should_display(node) then
-      nodes[#nodes + 1] = node
+    local group = {}
+    for _, item in ipairs(node) do
+      if Utils.option_should_display(item) then
+        group[#group + 1] = item
+      end
+    end
+    if #group > 0 then
+      if #nodes > 0 then
+        nodes[#nodes + 1] = { name = "separator" }
+      end
+      vim.list_extend(nodes, group)
     end
   end
 

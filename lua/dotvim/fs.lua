c41710a72132a40e0cc6017d5e1053e91aa28971
Hawtian Wang <twistoy.wang@gmail.com>
2025-04-24 10:25:59 +0800

    feat: add global read_file fn

--- lua/dotvim/fs.lua
+++ lua/dotvim/fs.lua
@@ -8,4 +8,21 @@ function M.direnv_root(bufnr)
   return root
 end
 
+---Read the entire content of a file
+---@param filepath string Path to the file
+---@return string? File content, returns nil if file doesn't exist or reading fails
+function M.read_file(filepath)
+  -- Check if file exists
+  local file = io.open(filepath, "r")
+  if not file then
+    return nil
+  end
+  
+  -- Read the entire file content
+  local content = file:read("*a")
+  file:close()
+  
+  return content
+end
+
 return M

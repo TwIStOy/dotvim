0978a99af59e0c88abd3350d6c358273c0de392d
Hawtian Wang <twistoy.wang@gmail.com>
2025-06-17 16:34:53 +0800

    fix: wip

--- lua/dotvim/configs/globals.lua
+++
@@ -1,9 +0,0 @@
----@module "dotvim.configs.globals"
-
--- set completion engine
-vim.g.dotvim_completion_engine = "blink-cmp"
-
--- check if macos and set curl path
-if vim.uv.os_uname().sysname == "Darwin" then
-  vim.g.plenary_curl_bin_path = "/opt/homebrew/opt/curl/bin/curl"
-end

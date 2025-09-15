55f689ee32235e43304d5f05cf269a9c43c5ccde
Hawtian Wang <twistoy.wang@gmail.com>
2025-09-15 17:27:49 +0800

    fix: typo

--- lua/dotvim/configs/copilot.lua
+++ lua/dotvim/configs/copilot.lua
@@ -37,7 +37,7 @@ vim.api.nvim_create_autocmd("BufEnter", {
   callback = function()
     vim.opt_local.relativenumber = false
     vim.opt_local.number = false
-    vim.opt_local.concelllevel = 0
+    vim.opt_local.conceallevel = 0
     vim.b.blink_pairs = false
   end,
 })

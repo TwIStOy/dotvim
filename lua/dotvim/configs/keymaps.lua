e807030f35fc5182363454ec0469a17b99a53f3e
Hawtian Wang <twistoy.wang@gmail.com>
2025-12-30 14:27:58 +0800

    refactor: merge keymaps into one file

--- lua/dotvim/configs/keymaps.lua
+++ lua/dotvim/configs/keymaps.lua
@@ -1,77 +1,50 @@
+local function disable_key(key)
+  vim.api.nvim_set_keymap("", key, "<Nop>", {})
+end
+
+local function nmap(key, cmd, desc)
+  vim.keymap.set("n", key, cmd, { desc = desc })
+end
+
 -- Disable arrow keys
-vim.api.nvim_set_keymap("", "<Left>", "<Nop>", {})
-vim.api.nvim_set_keymap("", "<Right>", "<Nop>", {})
-vim.api.nvim_set_keymap("", "<Up>", "<Nop>", {})
-vim.api.nvim_set_keymap("", "<Down>", "<Nop>", {})
+disable_key("<Left>")
+disable_key("<Right>")
+disable_key("<Up>")
+disable_key("<Down>")
 
 -- Basic file operations
-vim.keymap.set("n", "<leader>fs", "<cmd>update<CR>", { desc = "save" })
+nmap("<leader>fs", "<cmd>update<CR>", "save")
 
 -- Snacks picker keymaps (non-vscode)
 if not vim.g.vscode then
   local picker = require("dotvim.features.snacks_picker")
 
-  vim.keymap.set(
-    "n",
-    "<leader>e",
-    picker.find_files,
-    { desc = "edit-project-files" }
-  )
-  vim.keymap.set("n", "g/", picker.live_grep, { desc = "live-grep" })
-  vim.keymap.set(
-    "n",
-    "<leader>lg",
-    picker.live_grep,
-    { desc = "live-grep" }
-  )
-  vim.keymap.set("n", "<F4>", picker.buffers, { desc = "all-buffers" })
+  nmap("<leader>e", picker.find_files, "edit-project-files")
+  nmap("g/", picker.live_grep, "live-grep")
+  nmap("<leader>lg", picker.live_grep, "live-grep")
+  nmap("<F4>", picker.buffers, "all-buffers")
 end
 
 -- Clear search highlighting
-vim.keymap.set("n", "<M-n>", "<cmd>nohl<CR>", { desc = "nohl" })
+nmap("<M-n>", "<cmd>nohl<CR>", "nohl")
 
 -- Quit commands
-vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "quit" })
-vim.keymap.set("n", "<leader>Q", "<cmd>confirm qall<CR>", { desc = "quit-all" })
+nmap("<leader>q", "<cmd>q<CR>", "quit")
+nmap("<leader>Q", "<cmd>confirm qall<CR>", "quit-all")
 
 -- Window management (non-vscode)
 if not vim.g.vscode then
-  vim.keymap.set(
-    "n",
-    "<leader>wv",
-    "<cmd>wincmd v<CR>",
-    { desc = "split-window-vertical" }
-  )
-  vim.keymap.set(
-    "n",
-    "<leader>w-",
-    "<cmd>wincmd s<CR>",
-    { desc = "split-window-horizontal" }
-  )
-  vim.keymap.set(
-    "n",
-    "<leader>w=",
-    "<cmd>wincmd =<CR>",
-    { desc = "balance-window" }
-  )
-  vim.keymap.set(
-    "n",
-    "<leader>wr",
-    "<cmd>wincmd r<CR>",
-    { desc = "rotate-window-rightwards" }
-  )
-  vim.keymap.set(
-    "n",
-    "<leader>wx",
-    "<cmd>wincmd x<CR>",
-    { desc = "exchange-window-with-next" }
-  )
+  nmap("<leader>wv", "<cmd>wincmd v<CR>", "split-window-vertical")
+  nmap("<leader>w-", "<cmd>wincmd s<CR>", "split-window-horizontal")
+  nmap("<leader>w=", "<cmd>wincmd =<CR>", "balance-window")
+  nmap("<leader>wr", "<cmd>wincmd r<CR>", "rotate-window-rightwards")
+  nmap("<leader>wx", "<cmd>wincmd x<CR>", "exchange-window-with-next")
 
   -- Function to check if a window contains source code
   local function is_source_window(winid)
     local bufnr = vim.api.nvim_win_get_buf(winid)
-    local buftype = vim.api.nvim_buf_get_option(bufnr, "buftype")
-    local filetype = vim.api.nvim_buf_get_option(bufnr, "filetype")
+    local buftype = vim.api.nvim_get_option_value("buftype", { buf = bufnr })
+    local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
 
     -- Skip special buffer types
     if buftype ~= "" then
@@ -141,8 +114,41 @@ if not vim.g.vscode then
 
   -- Window navigation by index (only source windows)
   for i = 1, 9 do
-    vim.keymap.set("n", "<leader>" .. i, function()
+    nmap("<leader>" .. i, function()
       jump_to_source_window(i)
-    end, { desc = "jump-to-window-" .. i })
+    end, "jump-to-window-" .. i)
   end
+end
+
+-- LSP keymaps (non-vscode)
+if not vim.g.vscode then
+  local features = require("dotvim.features")
+
+  -- LSP method keybindings
+  nmap("gD", features.lsp_methods.declaration, "goto-declaration")
+  nmap("gd", features.lsp_methods.definitions, "goto-definitions")
+  nmap("gt", features.lsp_methods.type_definitions, "goto-type-definitions")
+  nmap("gi", features.lsp_methods.implementations, "goto-implementations")
+  nmap("gr", features.lsp_methods.references, "goto-references")
+  nmap("ga", features.lsp_methods.code_action, "code-action")
+  nmap("gR", features.lsp_methods.rename, "rename-symbol")
+  nmap("<leader>oi", features.lsp_methods.organize_imports, "organize-imports")
+  nmap("K", features.lsp_methods.show_hover, "show-hover")
+  nmap("<leader>ss", features.lsp_methods.document_symbols, "document-symbols")
+  nmap(
+    "<leader>sw",
+    features.lsp_methods.workspace_symbols,
+    "workspace-symbols"
+  )
+  nmap(
+    "<leader>sW",
+    features.lsp_methods.workspace_symbols_cword,
+    "workspace-symbols-cword"
+  )
+
+  -- Diagnostic navigation keybindings
+  nmap("]c", features.lsp_methods.next_diagnostic, "goto-next-error")
+  nmap("[c", features.lsp_methods.prev_diagnostic, "goto-prev-error")
+  nmap("]d", features.lsp_methods.next_diagnostic_all, "goto-next-diagnostic")
+  nmap("[d", features.lsp_methods.prev_diagnostic_all, "goto-prev-diagnostic")
 end

41dc1682cba0457080624fbb4de4b794cddd48bb
Hawtian Wang <twistoy.wang@gmail.com>
2026-01-20 12:09:40 +0800

    feat: add typescript language configuration with LSP and formatting

---
+++ lua/dotvim/plugins/langs/typescript.lua
@@ -0,0 +1,62 @@
+---@type LazyPluginSpec[]
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      lsp_configs = {
+        ts_ls = {
+          settings = {
+            typescript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+            javascript = {
+              inlayHints = {
+                includeInlayParameterNameHints = "all",
+                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+                includeInlayFunctionParameterTypeHints = true,
+                includeInlayVariableTypeHints = true,
+                includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+                includeInlayPropertyDeclarationTypeHints = true,
+                includeInlayFunctionLikeReturnTypeHints = true,
+                includeInlayEnumMemberValueHints = true,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        javascript = { "prettier" },
+        javascriptreact = { "prettier" },
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      return require("dotvim.commons").option.deep_merge(opts, {
+        extra = {
+          ensure_installed = {
+            "typescript-language-server",
+            "prettier",
+          },
+        },
+      })
+    end,
+  },
+}

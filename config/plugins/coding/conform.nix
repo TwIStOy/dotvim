87ea7df96d0bc7b2be4cd85202a0fcbc47602a2b
Hawtian Wang <twistoy.wang@gmail.com>
2026-05-08 00:36:52 +1000

    feat: add conform formatters from all langs to nixvim with nixpkgs packages

--- config/plugins/coding/conform.nix
+++ config/plugins/coding/conform.nix
@@ -1,4 +1,4 @@
-_: {
+{pkgs, lib, ...}: {
   plugins.conform-nvim = {
     enable = true;
     settings = {
@@ -6,6 +6,32 @@ _: {
         timeout_ms = 3000;
         async = false;
         quiet = false;
+      };
+      formatters_by_ft = {
+        lua = ["stylua"];
+        python = ["black"];
+        go = ["goimports" "gofumpt"];
+        sh = ["shfmt"];
+        nix = ["alejandra"];
+        cpp = ["clang_format"];
+        c = ["clang_format"];
+        rust = ["rustfmt"];
+        typescript = ["prettier"];
+        typescriptreact = ["prettier"];
+        javascript = ["prettier"];
+        javascriptreact = ["prettier"];
+        markdown = ["prettier"];
+      };
+      formatters = {
+        stylua.command = lib.getExe pkgs.stylua;
+        black.command = lib.getExe pkgs.black;
+        goimports.command = lib.getExe' pkgs.gotools "goimports";
+        gofumpt.command = lib.getExe pkgs.gofumpt;
+        shfmt.command = lib.getExe pkgs.shfmt;
+        alejandra.command = lib.getExe pkgs.alejandra;
+        clang_format.command = lib.getExe' pkgs.clang-tools "clang-format";
+        rustfmt.command = lib.getExe pkgs.rustfmt;
+        prettier.command = lib.getExe pkgs.prettier;
       };
     };
   };
